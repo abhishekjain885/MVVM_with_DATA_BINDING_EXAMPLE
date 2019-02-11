7be3a454d73abab1a2b8a4f096931113227b6370
Abhishek Jain <abhishek.jain@wavenetcorp.com>
2019-02-11 12:55:54 +0530

    Intial commit

---
+++ app/src/main/java/com/example/apple/minidemo/utils/Constant.kt
@@ -0,0 +1,10 @@
+package com.example.apple.minidemo.utils
+
+
+const val SOURCE_OBJECT = "source_object"
+const val BASE_URL = "https://newsapi.org/v2/"
+
+const val LANGUAGE = "en"
+
+const val API_KEY = "ENTER your key here"
+
